bb90f0977431a9c29219ced68f5152b0957cbfd9
Jakub Starzyński <starzynski96@gmail.com>
2020-06-21 12:41:18 +0200

    dodałem metdoę, która wyszukuje planetę po ID za pomocą @PathVariable

--- src/main/java/akademia/services/PlanetService.java
+++ src/main/java/akademia/services/PlanetService.java
@@ -1,7 +1,12 @@
 package akademia.services;
 
+import akademia.models.Planet;
 import akademia.repository.PlanetRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Service
 public class PlanetService {
@@ -9,5 +14,10 @@ public class PlanetService {
 
     public PlanetService(PlanetRepository planetRepository) {
         this.planetRepository = planetRepository;
+    }
+
+
+    public Optional<Planet> getPlanetByID (@PathVariable(value = "id") long id){
+        return planetRepository.findById(id);
     }
 }
