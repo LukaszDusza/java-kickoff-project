854c813e05034fcc772899bcb94af621365d5d85
AdrianMal <adrian.mal.92@gmail.com>
2020-06-21 13:00:19 +0200

    VIP: getPlanetByName

--- src/main/java/akademia/services/PlanetService.java
+++ src/main/java/akademia/services/PlanetService.java
@@ -1,7 +1,12 @@
 package akademia.services;
 
+
+import akademia.models.Planet;
 import akademia.repository.PlanetRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 public class PlanetService {
@@ -9,5 +14,9 @@ public class PlanetService {
 
     public PlanetService(PlanetRepository planetRepository) {
         this.planetRepository = planetRepository;
+    }
+
+    public List<Planet> getPlanetByName(String name){
+        return planetRepository.getPlanetByName(name);
     }
 }
