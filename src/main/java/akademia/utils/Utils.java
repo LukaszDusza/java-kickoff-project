0e62b1f367b29e2b2d330e0dfacf53f4370cec85
grzegorz <kaper46@gmail.com>
2020-06-21 11:31:37 +0200

    skonczone

--- src/main/java/akademia/utils/Utils.java
+++ src/main/java/akademia/utils/Utils.java
@@ -3,9 +3,9 @@ package akademia.utils;
 import akademia.models.Planet;
 
 public class Utils {
-public String  bottelCount (Planet planet){
+public Long  bottelCount (Planet planet){
   double bootel= 0.2 ;
-  long count;
-
+ long count;
+  return (long) (planet.getDistance()/bootel);
   }
 }
