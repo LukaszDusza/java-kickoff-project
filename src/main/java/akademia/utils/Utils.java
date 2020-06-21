66ef3332ca2826aa2b7a8868d85947eb38519211
AdrianMal <adrian.mal.92@gmail.com>
2020-06-21 11:33:13 +0200

    VIP: metoda ilosc ludzi

--- src/main/java/akademia/utils/Utils.java
+++ src/main/java/akademia/utils/Utils.java
@@ -6,7 +6,6 @@ public class Utils {
 
     public double getNumberOfPeople(Planet planet) {
             double avgHeight = 1.7; // [m]
-            double NumberOfPeople = planet.getDistance()/avgHeight;
-        return NumberOfPeople;
+        return planet.getDistance()/avgHeight;
     }
 }
