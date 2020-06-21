ae9460436e8148ae106e494c40efe61059b0ca06
AdrianMal <adrian.mal.92@gmail.com>
2020-06-21 11:12:19 +0200

    VIP: dokonczyc

---
+++ src/main/java/akademia/utils/Utils.java
@@ -0,0 +1,12 @@
+package akademia.utils;
+
+import akademia.models.Planet;
+
+public class Utils {
+
+    public double getNumberOfPeople(Planet planet) {
+            double height = 1.7; // [m]
+            double NumberOfPeople = /height;
+        return NumberOfPeople;
+    }
+}
