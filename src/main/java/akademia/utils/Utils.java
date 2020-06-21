8125aa33e4c01dcb3c750c06c711e731771b78ca
marcin <marcinsciborek@gmail.com>
2020-06-21 11:27:59 +0200

    skończyłem OlympicPool

--- src/main/java/akademia/utils/Utils.java
+++ src/main/java/akademia/utils/Utils.java
@@ -5,7 +5,7 @@ import akademia.models.Planet;
 public class Utils {
 
     public Double OlympicPool (Planet planet) {
-        double poolDistance = 25;
+        double poolDistance = 50;
         return planet.getDistance() / poolDistance;
 
     }
