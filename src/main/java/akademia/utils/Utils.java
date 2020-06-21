b21780d7d33ceba97b2bbf3550fe2f34b1214f91
maciek zelek <mzelechowski79@gmail.com>
2020-06-21 12:08:53 +0200

    dlugos w krokodylach poprawiono wciecia

--- src/main/java/akademia/utils/Utils.java
+++ src/main/java/akademia/utils/Utils.java
@@ -1,10 +1,11 @@
 package akademia.utils;
 // autor Maciej Żelechowski
+
 import akademia.models.Planet;
 
 public class Utils {
-  public long  crocodileLength (Planet planet){
-    final double crocodile= 4.37;
-    return (long) (planet.getDistance()/crocodile);
+  public long crocodileLength(Planet planet) {
+    final double crocodile = 4.37;
+    return (long) (planet.getDistance() / crocodile);
   }
 }
