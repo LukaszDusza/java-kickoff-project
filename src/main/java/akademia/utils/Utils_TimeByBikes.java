477fcbe53f89a4cc442af7043512ccab2113b360
stanislaw morozow <morozow.stanislaw@gmail.com>
2020-06-21 11:39:55 +0200

    WIP: Czas potrzebny na dojazd do planety w h

--- src/main/java/akademia/utils/Utils_TimeByBikes.java
+++ src/main/java/akademia/utils/Utils_TimeByBikes.java
@@ -2,7 +2,7 @@ package akademia.utils;
 
 import akademia.models.Planet;
 
-public class Utils extends Planet {
+public class Utils_TimeByBikes extends Planet {
 
      public double distanceByBikes(Planet planet) {
 
